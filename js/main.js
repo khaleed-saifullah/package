8c6d0aaa0f77477b4e31d9da5ba02749be2a6988
Khaleed saifullah <ksnayem@gmail.com>
2022-06-28 23:10:38 +0600

    design issues fix

--- js/main.js
+++ js/main.js
@@ -121,3 +121,25 @@ $(document).ready(function () {
     $(".trust-select-cat").select2();
     $(".feature-select-cat").select2();
 });
+
+jQuery(function ($) {
+    function tog(v) {
+        return v ? "addClass" : "removeClass";
+    }
+
+    $(document)
+        .on("input", ".clearable", function () {
+            $(this)[tog(this.value)]("x");
+        })
+        .on("mousemove", ".x", function (e) {
+            $(this)[
+                tog(
+                    this.offsetWidth - 25 <
+                        e.clientX - this.getBoundingClientRect().left
+                )
+            ]("onX");
+        })
+        .on("click", ".onX", function () {
+            $(this).removeClass("x onX").val("");
+        });
+});
